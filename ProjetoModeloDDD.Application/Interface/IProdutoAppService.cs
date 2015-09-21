bb4b40987a1859f46d362ea025461032cb2342da
Mesquita <mesquitta@gmail.com>
2015-09-21 19:13:44 -0300

    Iniciando a camada de Aplicação.

---
+++ ProjetoModeloDDD.Application/Interface/IProdutoAppService.cs
@@ -0,0 +1,15 @@
+﻿using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoModeloDDD.Application
+{
+    public interface IProdutoAppService : IServiceBase<Produto>
+    {
+
+    }
+}
